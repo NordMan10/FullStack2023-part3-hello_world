09696400dca3a333ef7b50fc5fe31ba69c75cc99
Yury <yury.ten66@yandex.ru>
2023-10-06 21:24:21 +0300

    uibuild

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const unknownEndPoint = (request, response) => {
 
 app.use(cors())
 app.use(express.json())
-app.use(requestLogger)
+// app.use(requestLogger)
 app.use(express.static('dist'))
 
 let notes = [
